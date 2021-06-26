44d17636bba75856dfcc26ad26e7138a6cc3810d
GayatriVishwakarma <44763595+GayatriVishwakarma@users.noreply.github.com>
2021-06-26 22:03:15 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic web development javascript with react js</p>
+    </div>
+  );
+}
+export default Info;
